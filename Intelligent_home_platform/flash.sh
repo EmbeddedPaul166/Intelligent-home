a354c0ccb55dbe3068974ceb88d6d8b408101435
Paul <p.pietrzak166@gmail.com>
2019-10-31 19:07:30 +0100

    Blink sample codde, flash script, libc fix
    LED2 doesn't blink after upload, debug is needed

---
+++ Intelligent_home_platform/flash.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+st-flash write bin/home_management_system.bin 0x8000000
--- Intelligent_home_platform/src/main.c
+++ Intelligent_home_platform/src/main.c
@@ -1,9 +1,54 @@
 #include <stdint.h>
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_nucleo.h"
+
+static GPIO_InitTypeDef  GPIO_InitStruct;
+void SystemClock_Config(void);
 
 int main()
 {
-    int a = 5;
-    int b = 5;
-    int c = a*b;
+    HAL_Init();
+    SystemClock_Config();
+    LED2_GPIO_CLK_ENABLE();
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull  = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Pin = LED2_PIN;
+    HAL_GPIO_Init(LED2_GPIO_PORT, &GPIO_InitStruct);
+    while (1)
+    {
+        HAL_GPIO_TogglePin(LED2_GPIO_PORT, LED2_PIN);
+        HAL_Delay(500);
+    } 
     return 0;
+}
+
+void SystemClock_Config(void)
+{
+    RCC_ClkInitTypeDef clkinitstruct = {0};
+    RCC_OscInitTypeDef oscinitstruct = {0};
+
+    oscinitstruct.OscillatorType  = RCC_OSCILLATORTYPE_HSI;
+    oscinitstruct.HSEState        = RCC_HSE_OFF;
+    oscinitstruct.LSEState        = RCC_LSE_OFF;
+    oscinitstruct.HSIState        = RCC_HSI_ON;
+    oscinitstruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    oscinitstruct.HSEPredivValue    = RCC_HSE_PREDIV_DIV1;
+    oscinitstruct.PLL.PLLState    = RCC_PLL_ON;
+    oscinitstruct.PLL.PLLSource   = RCC_PLLSOURCE_HSI_DIV2;
+    oscinitstruct.PLL.PLLMUL      = RCC_PLL_MUL16;
+    if (HAL_RCC_OscConfig(&oscinitstruct)!= HAL_OK)
+    {
+        while(1); 
+    }
+
+    clkinitstruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+    clkinitstruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    clkinitstruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    clkinitstruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    clkinitstruct.APB1CLKDivider = RCC_HCLK_DIV2;  
+    if (HAL_RCC_ClockConfig(&clkinitstruct, FLASH_LATENCY_2)!= HAL_OK)
+    {
+        while(1); 
+    }
 }
